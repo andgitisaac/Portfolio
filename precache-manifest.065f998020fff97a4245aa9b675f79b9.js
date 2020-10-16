857ae577c481500646d7350874e7fe8be2dde58b
andgitisaac <hjpeng.isaac@gmail.com>
2020-10-15 21:00:44 -0700

    Updates

---
+++ precache-manifest.065f998020fff97a4245aa9b675f79b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9249b219197b51fd1303dc887a872f7c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9d38658e925f3c1b7993",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7357af019cfbac6eb3cf",
+    "url": "/Portfolio/static/js/2.9e7789ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Portfolio/static/js/2.9e7789ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d38658e925f3c1b7993",
+    "url": "/Portfolio/static/js/main.0e65fed2.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
